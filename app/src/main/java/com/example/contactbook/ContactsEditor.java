406a2e2a877389da4c2bbe47214ca5bf5915a5e9
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-02-07 23:52:07 -0500

    New edits

---
+++ app/src/main/java/com/example/contactbook/ContactsEditor.java
@@ -0,0 +1,12 @@
+package com.example.contactbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsEditor extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts_editor);
+    }
+}
